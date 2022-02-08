63b1d0ab0805aa15811d81ddd8d9ed9d1ec1106c
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-08 20:17:53 +0100

    added feature that automatically generate rows based on the number of seats

--- src/App.js
+++ src/App.js
@@ -3,53 +3,20 @@ import "./App.css";
 import { Seat } from "./components/Seat";
 
 function App() {
+  const numberOfSeats = 28;
   return (
     <div className="train-carriage">
-      <div className="carriage-row">
-        <Seat number="1" availability="occupied" />
-        <Seat number="2" availability="free" />
+      {new Array(numberOfSeats / 4).fill(null).map((element, row) => (
+        <div key={row} className="carriage-row">
+          <Seat number={row * 4 + 1} />
+          <Seat number={row * 4 + 2} />
 
-        <div className="aisle-spacer"></div>
+          <div className="aisle-spacer"></div>
 
-        <Seat number="3" availability="free" />
-        <Seat number="4" availability="selected" />
-      </div>
-      <div className="carriage-row">
-        <Seat number="5" availability="occupied" />
-        <Seat number="6" availability="occupied" />
-
-        <div className="aisle-spacer"></div>
-
-        <Seat number="7" availability="free" />
-        <Seat number="8" availability="free" />
-      </div>
-      <div className="carriage-row">
-        <Seat number="8" availability="free" />
-        <Seat number="9" availability="occupied" />
-
-        <div className="aisle-spacer"></div>
-
-        <Seat number="10" availability="free" />
-        <Seat number="11" availability="selected" />
-      </div>
-      <div className="carriage-row">
-        <Seat number="12" availability="occupied" />
-        <Seat number="13" availability="selected" />
-
-        <div className="aisle-spacer"></div>
-
-        <Seat number="14" availability="free" />
-        <Seat number="15" availability="selected" />
-      </div>
-      <div className="carriage-row">
-        <Seat number="16" availability="occupied" />
-        <Seat number="17" availability="selected" />
-
-        <div className="aisle-spacer"></div>
-
-        <Seat number="18" availability="free" />
-        <Seat number="19" availability="occupied" />
-      </div>
+          <Seat number={row * 4 + 3} />
+          <Seat number={row * 4 + 4} />
+        </div>
+      ))}
     </div>
   );
 }
